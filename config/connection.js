cc5da93c2af47d30ccab136ea10187aaee7d43c9
suarezluis <suarezluis@gmail.com>
2018-05-29 20:23:07 -0500

    added security to db info

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
 
-var connection = mysql.createConnection({
+else {var connection = mysql.createConnection({
   host: "zf4nk2bcqjvif4in.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   user: "xl49j9mdhd7w6gmj",
   password: "sqjj81vtsyto5ppg",
   database: "n1dwx0v0u5qx2hlg"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
